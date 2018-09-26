bbe3029264912ac4c3e19ff7441b6312fac575be
wuge821 <50392058@qq.com>
2018-09-26 10:29:41 +0800

    a

---
+++ 1.c/1.c/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("您好，我是C#");
+            Console.ReadKey();
+        }
+    }
+}
